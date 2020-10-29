5c70eb505f9736b623c04a5799d3a64fc1535f5c
Maxim <maxgpod@gmail.com>
2020-10-29 14:28:18 -0700

    Two Sum

---
+++ 6kyu/Two_Sum.js
@@ -0,0 +1,31 @@
+/*
+
+https://www.codewars.com/kata/52c31f8e6605bcc646000082
+
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+Based on: http://oj.leetcode.com/problems/two-sum/
+
+twoSum [1, 2, 3] 4 === (0, 2)
+
+ */
+
+function twoSum(numbers, target) {
+    let numbers2 = numbers;
+    let result = [];
+    let sum = 0;
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = 1; j < numbers2.length; j++) {
+            sum = numbers[i] + numbers2[j]
+            if (sum === target) {
+                result.push(i)
+                result.push(j)
+                break;
+            }
+        }
+    } return result.slice(0, 2);
+}
