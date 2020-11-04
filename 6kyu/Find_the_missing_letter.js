4ca1a7543539e817a092c1577a542e86ef4539f0
Maxim <maxgpod@gmail.com>
2020-11-04 14:59:04 -0800

    Find the missing number

---
+++ 6kyu/Find_the_missing_letter.js
@@ -0,0 +1,28 @@
+/*
+
+https://www.codewars.com/kata/5839edaa6754d6fec10000a2
+
+#Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+["a","b","c","d","f"] -> "e"
+["O","Q","R","S"] -> "P"
+(Use the English alphabet with 26 letters!)
+ */
+
+function findMissingLetter(array)
+{
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].charCodeAt(0) !== array[i+1].charCodeAt(0)-1) {
+            return String.fromCharCode(array[i+1].charCodeAt(0)-1);
+        }
+    }
+}
