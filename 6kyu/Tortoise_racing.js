90f5c431feb998822cad3ba2f08a70b6711ad18b
Maxim <maxgpod@gmail.com>
2020-11-14 20:53:47 -0800

    Tortoise Racing

---
+++ 6kyu/Tortoise_racing.js
@@ -0,0 +1,27 @@
+/*
+
+https://www.codewars.com/kata/55e2adece53b4cdcb900006c
+
+Two tortoises named A and B must run a race. A starts with an average speed of 720 feet per hour. Young B knows she runs faster than A, and furthermore has not finished her cabbage.
+
+When she starts, at last, she can see that A has a 70 feet lead but B's speed is 850 feet per hour. How long will it take B to catch A?
+
+More generally: given two speeds v1 (A's speed, integer > 0) and v2 (B's speed, integer > 0) and a lead g (integer > 0) how long will it take B to catch A?
+
+The result will be an array [hour, min, sec] which is the time needed in hours, minutes and seconds (round down to the nearest second) or a string in some languages.
+
+If v1 >= v2 then return nil, nothing, null, None or {-1, -1, -1} for C++, C, Go, Nim, [] for Kotlin or "-1 -1 -1".
+
+
+ */
+
+function race(v1, v2, g) {
+    if (v1 >= v2) return null;
+    let v3 = v2 - v1;
+    let time = g * 60 * 60 / v3;
+    let arr2 = [];
+    arr2.push(Math.trunc(time / 3600));
+    arr2.push(Math.trunc(time % 3600 / 60));
+    arr2.push(Math.trunc(((time % 3600) % 60)));
+    return arr2;
+}
--- 6kyu/Unique_in_order.js
+++ 6kyu/Unique_in_order.js
@@ -10,7 +10,7 @@ uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
  */
 
-var uniqueInOrder=function(iterable){
+var uniqueInOrder = function(iterable){
     let result = [...iterable].filter((a, i) => a !== iterable[i - 1]);
     return result;
 }
