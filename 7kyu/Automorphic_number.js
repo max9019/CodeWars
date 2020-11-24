f1e3f0ec0fce7399a2e8310a1ddd25402ce23758
Maxim <maxgpod@gmail.com>
2020-11-23 23:54:35 -0800

    Automorphic number

---
+++ 7kyu/Automorphic_number.js
@@ -0,0 +1,17 @@
+/*
+https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/javascript
+
+A number is called Automorphic number if and only if its square ends in the same digits as the number itself.
+
+Task
+Given a number determine if it Automorphic or not .
+
+ */
+
+function automorphic(n){
+    let arr = n.toString().split('');
+    let n2 = n * n;
+    let arr2 = n2.toString().split('');
+    arr2 = arr2.slice(-arr.length, arr2.length);
+    return +arr2.join('') === n ? "Automorphic" : "Not!!";
+}
