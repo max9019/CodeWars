3f904d7826d44ef9f368652f21ec49065b6a1c77
Maxim <maxgpod@gmail.com>
2020-11-27 15:58:37 -0800

    getNames()

---
+++ 7kyu/getNames().js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/514a677421607afc99000002/train/javascript
+
+The following code is not giving the expected results. Can you figure out what the issue is?
+
+The following is an example of data that would be passed in to the function.
+
+var data = [
+  {name: 'Joe', age: 20},
+  {name: 'Bill', age: 30},
+  {name: 'Kate', age: 23}
+]
+
+getNames(data) // should return ['Joe', 'Bill', 'Kate']
+ */
+
+function getNames(data){
+    return data.map(el => el = el.name);
+}
