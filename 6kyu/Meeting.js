220c5acc38ab5154c89bef3f5c002fdc5f9deaba
Maxim <maxgpod@gmail.com>
2020-11-29 12:30:13 -0800

    Meeting

---
+++ 6kyu/Meeting.js
@@ -0,0 +1,30 @@
+/*
+https://www.codewars.com/kata/59df2f8f08c6cec835000012/train/javascript
+
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result
+between parentheses separated by a comma.
+
+
+ */
+
+function meeting(s) {
+    s = s.toUpperCase();
+    let arr = s.split(';').map(el => el.replace(':', ' '));
+    function compareFirst(a, b){
+        a = a.split(' ')[0];
+        b = b.split(' ')[0];
+        return a > b ? 1 : -1;
+    }
+    let lastNames = arr.map(el => el.split(' ')[1]);
+    lastNames = lastNames.filter((el, i) => i === lastNames.lastIndexOf(el)).sort();
+    let result = '';
+    for (let i = 0; i < lastNames.length; i++) {
+        let tempArr = arr.filter(el => el.split(' ')[1] === lastNames[i]);
+        tempArr.sort(compareFirst);
+        console.log(tempArr);
+        for (let j = 0; j < tempArr.length; j++) {
+            result += `(${tempArr[j].split(' ')[1]}, ${tempArr[j].split(' ')[0]})`;
+        }
+    }
+    return result;
+}
