cae178f494e64cead7fcd125c91d5ba3def3585b
Maxim <maxgpod@gmail.com>
2020-12-18 22:25:31 -0800

    All star code challenge #1

---
+++ 7kyu/All_star_code_challenge_#1.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/5863f97fb3a675d9a700003f/train/javascript
+
+All Star Code Challenge #1
+
+Write a function, called sumPPG, that takes two NBA player objects and sums their PPG
+
+ */
+
+function sumPPG(playerOne, playerTwo){
+    return playerOne.ppg + playerTwo.ppg;
+}
