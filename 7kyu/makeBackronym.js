d6428c4c08513a26cf550f19afba685ff6ff7da6
Maxim <maxgpod@gmail.com>
2020-12-18 22:27:44 -0800

    makeBackronym

---
+++ 7kyu/makeBackronym.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/55805ab490c73741b7000064/train/javascript
+
+Complete the function to create backronyms.
+Transform the given string (without spaces) to a backronym, using the preloaded dictionary and return a
+string of words, separated with a single space (but no trailing spaces).
+
+The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words,
+for example:
+ */
+
+//preload variable: dict
+
+var makeBackronym = function(string){
+    //your code here
+};
