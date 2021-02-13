5002e158a92854be2f514e401b08f56782f5a1c5
Maxim <maxgpod@gmail.com>
2021-02-13 14:24:00 -0800

    Anagram detection

---
+++ 7kyu/Anagram_detection.js
@@ -0,0 +1,24 @@
+/*
+https://www.codewars.com/kata/529eef7a9194e0cbc1000255/solutions/javascript/me/best_practice
+
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+ */
+
+function isAnagram (test, original) {
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+}
+
+var isAnagram = function(test, original) {
+    test = test.toLowerCase().split('').sort();
+    original = original.toLowerCase().split('').sort();
+    for (let i = 0; i < original.length; i++) {
+        if (test[i] !== original[i]) {
+            return false;
+        }
+    }
+    return test.length === original.length ? true : false;
+};
